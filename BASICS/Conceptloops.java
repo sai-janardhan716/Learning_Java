33a0fc7143087491cb3d12e88f6f8a99d6db0c01
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-04 09:52:14 +0530

    Practice problems added

--- BASICS/Conceptloops.java
+++ BASICS/Conceptloops.java
@@ -102,6 +102,63 @@ public class Conceptloops {
         -> do-while   → condition-based, runs at least once.
 
     */
-        
+    
+    // Practice Problems
+
+    //problem 1 - STAR Pattern
+        // for(int i=4;i>=0;i--){
+        //     for (int j= 0;j<=i;j++){
+        //         System.out.print('*');
+        //     }
+        //     System.out.println();
+        // }
+
+        //problem 2 - sum of even numbers
+        // int n = 10;
+        // int i=0;
+        // int result = 0;
+        // while(i<=n){
+        //     result+=2*i;
+        //     i++;
+        // }
+        // System.out.println(result);
+
+        //problem 3 - multiplication table\
+        // int n = 10;
+        // for(int i = 1;i<=n;i++){
+        //     System.out.println(n + " * " + i + " = "+n*i);
+        // }
+
+        //problem4-multiplication table in reverse order
+        // int n = 10;
+        // for (int i=n;i>0;i--){
+        //     System.out.println(n + " * " + i + " = "+n*i);
+        // }
+
+        //problem 5 - factorial of a given number
+        // int n = 4;
+        // int result=1;
+        // for(int i=n;i>0;i--){
+        //     result*=i;
+        // }
+        // System.out.println(result);
+
+        //problem 6 - repeat 5 using while loop
+        // while(true){
+        //     System.out.println(5);
+        // }
+
+        //problem 7 - repeat 5 using for loop
+        // for(int i =10;i!=0;i++){
+        //     System.out.println(5);
+        // }
+
+        //problem 8 - sum of numbers occured in multiplication table of 2
+        // int n =2;
+        // int result = 0;
+        // for(int i=1;i<=10;i++){
+        //     result+=n*i;
+        // }
+        // System.out.println(result);
     }
 }
