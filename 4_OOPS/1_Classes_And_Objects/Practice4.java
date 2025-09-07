c1c597629376233c21bb2364b857c2868af3a36b
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-07 20:45:15 +0530

    Prblem1 Solution added

--- 4_OOPS/1_Classes_And_Objects/Practice4.java
+++ 4_OOPS/1_Classes_And_Objects/Practice4.java
@@ -9,5 +9,27 @@ public class Practice4 {
         4) Create a class 'Car' with fields brand, model, and year. Add a method to display car details.
         5) Create a class 'BankAccount' with fields accountNumber and balance. Add methods deposit() and withdraw().
     */
+
+    // Solutions
+
+/*
+    1)  class Student {
+            String name;
+            int age;
+            char grade;
+            void display() {
+                System.out.println("Name: " + name + ", Age: " + age + ", Grade: " + grade);
+            }
+        }
+        public class Main {
+            public static void main(String[] args) {
+                Student s = new Student();
+                s.name = "Sai";
+                s.age = 21;
+                s.grade = 'A';
+                s.display();
+            }
+        }
+*/
     }
 }
