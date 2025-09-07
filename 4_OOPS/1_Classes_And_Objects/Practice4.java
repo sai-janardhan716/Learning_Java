62f6d3babb18bca167873e28949c173fc497047a
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-07 20:43:54 +0530

    Proble,s for classes and objects added

---
+++ 4_OOPS/1_Classes_And_Objects/Practice4.java
@@ -0,0 +1,13 @@
+public class Practice4 {
+    public static void main(String[] args) {
+    // Practice Problems
+
+    /* 
+        1) Create a class 'Student' with fields name, age, and grade. Write a method to display student details.
+        2) Create a class 'Calculator' with methods add, subtract, multiply, divide.Test using objects.
+        3) Write a class 'Rectangle' with fields length & breadth. Write methods to calculate area and perimeter.
+        4) Create a class 'Car' with fields brand, model, and year. Add a method to display car details.
+        5) Create a class 'BankAccount' with fields accountNumber and balance. Add methods deposit() and withdraw().
+    */
+    }
+}
