0ba4f7788d90cca2b1e8b7be20e0f7a586320d27
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-08 20:28:39 +0530

    Methods concept added

---
+++ 4_OOPS/1_Methods/Methods.java
@@ -0,0 +1,66 @@
+public class Methods {
+    public static void main(String[] args) {
+
+    /*
+    Functions in Java
+    -----------------
+    -> In Java, functions are called METHODS. 
+    -> A method is a block of code that performs a specific task and can be reused.
+    -> To avoid code repetition.
+    -> To increase code readability and reusability.
+    -> To divide large programs into smaller manageable parts.
+
+    Syntax of a Method  (You can learn more about return in future)
+    ----------------------
+    returnType methodName(parameters) {
+        // body of the method
+        return value;   // if returnType is not void
+    }
+
+    Example:
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    Types of Methods
+    --------------------
+    a) Predefined (Library) Methods
+        -> Already defined in Java libraries.
+        -> Example: 
+           Math.max(10, 20);  // returns 20
+           System.out.println("Hello Janardhan"); // println is predefined
+
+    b) User-defined Methods
+        -> Created by programmer.
+        -> Example:
+           int square(int num) {
+               return num * num;
+           }
+
+    Static vs Instance Methods      (You can learn more about this in oops concept)
+    ------------------------------
+    -> static methods: belong to the class, can be called without creating an object.
+       Example: Math.sqrt(16);
+    -> instance methods: require object creation.
+       Example:
+            class Car {
+                void drive() {
+                    System.out.println("Car is driving...");
+                }
+            }
+            Car obj = new Car();
+            obj.drive();
+    Method Parameters
+    --------------------
+    -> Pass values to methods using parameters.
+    -> Parameters act as variables inside the method.
+
+    Example:
+    void greet(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    */
+
+    }
+}
