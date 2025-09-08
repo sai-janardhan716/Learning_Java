16adb00668c361574cd3bee7a33dc3f9f0f27c63
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-08 20:34:47 +0530

    Problem 1 solution added

---
+++ 4_OOPS/1_Methods/Practice.java
@@ -0,0 +1,32 @@
+public class Practice {
+    public static void main(String[] args) {
+    /* 
+        Practice Problems
+        -----------------------------
+        1) Write a function to calculate the factorial of a number.
+        2) Create a function to check if a number is prime.
+        3) Write a method that takes a string and returns it in reverse.
+        4) Write a method which returns the sum of all elements in the array.
+        5) Create a method to find the maximum element in an array.
+    */
+    /* 
+        //Solutions
+
+        1) Factorial of a number
+        class Factorial {
+            static int factorial(int n) {
+                int fact = 1;
+                for(int i = 1; i <= n; i++) {
+                    fact *= i;
+                }
+                return fact;
+            }
+        }
+        public class Main {
+            public static void main(String[] args) {
+                System.out.println("Factorial of 5: " + Factorial.factorial(5));
+            }
+        }
+    */
+    }
+}
