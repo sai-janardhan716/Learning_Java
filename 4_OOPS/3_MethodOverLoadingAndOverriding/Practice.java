c6c76a0fa821a6afe58aff4f8f81e9eb82a3eaf9
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-09 11:15:34 +0530

    Problem 3 solution added

--- 4_OOPS/3_MethodOverLoadingAndOverriding/Practice.java
+++ 4_OOPS/3_MethodOverLoadingAndOverriding/Practice.java
@@ -56,6 +56,17 @@ public class Practice {
         }
     }
 
+    3) class Greeter {
+        void greet() {
+            System.out.println("Hello");
+        }
+        void greet(String name) {
+            System.out.println("Hello, " + name);
+        }
+        void greet(String name, int age) {
+            System.out.println("Hello, " + name + ", age: " + age);
+        }
+    }
 
     */
 
