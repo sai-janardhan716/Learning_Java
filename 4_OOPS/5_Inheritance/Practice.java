aa07766817efe5119f642ac358859cbb8f4a2d09
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-11 10:46:26 +0530

    Problem 1 solution

--- 4_OOPS/5_Inheritance/Practice.java
+++ 4_OOPS/5_Inheritance/Practice.java
@@ -9,5 +9,24 @@ public class Practice {
             4) Create a class Shape with a method area(). Inherit Circle and Rectangle classes and calculate their areas.
             5)  Create a class Employee with a method work(). Inherit Manager and Developer classes and override work() to display their specific tasks.
         */
+
+        //Solutions
+        /* 
+        !) class Animal {
+            void sound() {
+                System.out.println("Animals make sounds");
+            }
+        }
+        class Dog extends Animal {
+            void sound() {
+                System.out.println("Dog barks");
+            }
+        }
+        class Cat extends Animal {
+            void sound() {
+                System.out.println("Cat meows");
+            }
+        }
+        */
     }
 }
