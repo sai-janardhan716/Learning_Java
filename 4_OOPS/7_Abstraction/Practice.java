1d97757ccbd078a31667db4187d82a045d923b4c
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-13 12:07:07 +0530

    Problem 1 solution added

---
+++ 4_OOPS/7_Abstraction/Practice.java
@@ -0,0 +1,35 @@
+public class Practice {
+    public static void main(String[] args) {
+    //Problems
+    /*
+        1) Create an abstract class Vehicle with abstract method startEngine().
+        Extend it in Car and Bike classes and implement startEngine() differently.
+
+        2) Define an interface Shape with method area().
+        Implement it in Circle and Rectangle classes.
+
+        3) Create an interface Playable with method play().
+        Implement in Guitar and Piano classes.
+
+        4) Make an abstract class Employee with abstract method calculateSalary().
+        Extend it to FullTimeEmployee and PartTimeEmployee classes.
+    */
+
+    /*
+        1) abstract class Vehicle{
+            abstract void startEngine();
+        }
+        class Car extends Vehicle{
+            void startEngine(){
+                System.out.println("Car engine starts with key");
+            }
+        }
+        class Bike extends Vehicle{
+            void startEngine(){
+                System.out.println("Bike engine starts with kick");
+            }
+        }
+    */
+
+    }
+}
