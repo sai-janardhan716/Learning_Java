4309795d80a6e899986694cb3018aae708ea1d4e
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-14 20:28:17 +0530

    Problem 1 solution added

--- 4_OOPS/8_Encapsulation/Practice.java
+++ 4_OOPS/8_Encapsulation/Practice.java
@@ -40,5 +40,35 @@ public class Practice {
             -> "Fail" otherwise.
     */
 
+    //Solutions
+    /*
+        class BankAccount{
+        private String accountNumber;
+        private double balance;
+        BankAccount(String accountNumber, double balance){
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+        }
+        public String getAccountNumber(){
+            return accountNumber;
+        }
+        public double getBalance(){
+            return balance;
+        }
+        public void deposit(double amount){
+            if (amount > 0) {
+                balance += amount;
+            }
+        }
+
+        public void withdraw(double amount){
+            if (amount > 0 && balance >= amount){
+                balance -= amount;
+            } else {
+                System.out.println("Insufficient balance or invalid amount");
+            }
+        }
+    }
+    */
     }
 }
