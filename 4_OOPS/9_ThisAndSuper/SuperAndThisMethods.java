0ce21ba179af7f54f94e4252b4f3c60364e44435
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-15 09:47:37 +0530

    This And Super concepts added

---
+++ 4_OOPS/9_ThisAndSuper/SuperAndThisMethods.java
@@ -0,0 +1,40 @@
+// In default Every constructor has supermethod (super())
+// This "super()" method used to call and execute the superclass
+// Now there is a method called this() which refers to the constructor in that class only
+
+class A extends Object{
+    public A(){
+        // super();  --> We dont have to mention it becuase by default it was there
+        System.out.println("In the constuctor A");
+    }
+
+    public A(int n){
+        this();
+        System.out.println("In the parameterised constructor of A");
+    }
+}
+
+class B extends A{
+    public B(){
+        super(2);
+        System.out.println("In the constructor B");
+    }
+
+    public B(int n){
+        this();
+        System.out.println("In the parameterised constructor B");
+    }
+}
+public class SuperAndThisMethods {
+    public static void main(String[] args){
+        B obj = new B(2);
+
+        /*After the object creation of B the constructor of 'B' has been executed.
+          In the 'B. constructor the super() method was there, so it executes the constructor of the superclass which is class "A"
+          But in the constructor 'A' super() method was there, Basically every class in java inherits from "Object" class which is default.
+          In that "object" class there is a default constructor.
+          so First the constructor in "Object" class has been executed and the constructor in Class "A" and then Constroctur in class "B"
+        */
+
+    }
+}
