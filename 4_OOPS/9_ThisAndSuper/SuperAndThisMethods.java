92b7a395feeb03e07f85f4d02eba7ee710f65bc3
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-15 09:53:24 +0530

    This And Super keywordss contents added

--- 4_OOPS/9_ThisAndSuper/SuperAndThisMethods.java
+++ 4_OOPS/9_ThisAndSuper/SuperAndThisMethods.java
@@ -41,3 +41,99 @@ public class SuperAndThisMethods {
 
     }
 }
+//Keywords
+
+// Both this and super are reference keywords used to refer to objects, but they serve different purposes.
+
+/*
+    ------------------
+    The this Keyword
+    ------------------
+    -> Refers to the current object of the class.
+    -> Common Uses:
+        1) To refer to the current class instance variable.
+        2) To call another constructor of the same class (constructor chaining).
+        3) To pass the current object as an argument in a method or constructor.
+        4) To return the current class instance from a method.
+*/
+
+// Example 1: Referring to current class variables
+class Student {
+    private String name;
+    private int age;
+
+    Student(String name, int age) {
+        this.name = name;  // this differentiates between instance variables and parameters
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println("Name: " + this.name + ", Age: " + this.age);
+    }
+}
+
+// public class MainThis {
+//     public static void main(String[] args) {
+//         Student s = new Student("Sai", 20);
+//         s.display();
+//     }
+// }
+
+/*
+    The super Keyword
+    ------------------
+    -> Refers to the immediate parent (superclass) object.
+    -> Common Uses:
+        1) To access parent class variables if they are hidden by child class variables.
+        2) To call parent class methods when overridden in child class.
+        3) To call parent class constructor from a child class constructor.
+*/
+
+// Example 2: Accessing parent class members
+class Animal {
+    String type = "Generic Animal";
+    void sound() {
+        System.out.println("Some generic animal sound");
+    }
+}
+
+class Dog extends Animal {
+    String type = "Dog";
+
+    void displayType() {
+        System.out.println("Child type: " + type);
+        System.out.println("Parent type: " + super.type); // using super to access parent variable
+    }
+
+    @Override
+    void sound() {
+        super.sound(); // calls parent class method
+        System.out.println("Dog barks");
+    }
+}
+
+// public class MainSuper {
+//     public static void main(String[] args) {
+//         Dog d = new Dog();
+//         d.displayType();
+//         d.sound();
+//     }
+// }
+
+/*
+    Key Differences
+    ------------------------
+    -> this :
+         - Refers to the current class instance.
+         - Used for accessing current class variables, methods, and constructors.
+    -> super :
+         - Refers to the immediate parent class instance.
+         - Used for accessing parent class variables, methods, and constructors.
+*/
+
+/*
+    Summary
+    -------
+    1) Use this when you need to reference the current object.
+    2) Use super when you need to reference members of the parent class.
+*/
