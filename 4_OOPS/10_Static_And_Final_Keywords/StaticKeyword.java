78b02d19f3f36c7c5310cf53e2d7fd046f428200
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-16 18:07:11 +0530

    Updated

--- 4_OOPS/10_Static_And_Final_Keywords/StaticKeyword.java
+++ 4_OOPS/10_Static_And_Final_Keywords/StaticKeyword.java
@@ -33,32 +33,33 @@ public class StaticKeyword {
         */
 
         /*
-        b) Static Methods:-
-        --------------------
-        -> Belong to the class, not to a specific object.
-        -> Can be called without creating an object of the class.
-        -> Cannot access non-static (instance) members directly, because there is no specific object to refer to.
-        -> Used for utility/helper methods.
+            b) Static Methods:-
+            --------------------
+            -> Belong to the class, not to a specific object.
+            -> Can be called without creating an object of the class.
+            -> Cannot access non-static (instance) members directly, because there is no specific object to refer to.
+            -> Used for utility/helper methods.
 
-        Example:-
-        class MathUtil{
-            static int square(int x){      // here square method is a static method
-                return x  x;
+            Example:-
+            class MathUtil{
+                static int square(int x){      // here square method is a static method
+                    return x  x;
+                }
             }
-        }
-        public class Main{
-            public static void main(String[] args) {
-                int result = MathUtil.square(5);    // called using class name
-                System.out.println("Square = " + result);
+            public class Main{
+                public static void main(String[] args) {
+                    int result = MathUtil.square(5);    // called using class name
+                    System.out.println("Square = " + result);
+                }
             }
-        }
 
-        -> Here square() is a static method, so we can call it as MathUtil.square(5) without creating a MathUtil object.
-        -> It can directly use other static variables or call other static methods of the class.
+            -> Here square() is a static method, so we can call it as MathUtil.square(5) without creating a MathUtil object.
+            -> It can directly use other static variables or call other static methods of the class.
 
-        Note:-
-        ------
-        -> A static method cannot use this or super because it does not belong to any particular object instance.
+            Note:-
+            ------
+            -> A static method cannot use this or super because it does not belong to any particular object instance.
         */
+        
     }
 }
