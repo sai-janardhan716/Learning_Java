33b56a60a05511c4baaa1397bd63b0bacbb8aa1a
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-17 11:19:43 +0530

    Final Keyword concept added

---
+++ 4_OOPS/10_Static_And_Final_Keywords/FinalKeyword.java
@@ -0,0 +1,8 @@
+public class FinalKeyword {
+    public static void main(String[] args) {
+        /*
+            -> The final keyword in Java is used to create constants, prevent method overriding, and stop inheritance of a class. 
+            -> It can be applied to variables, methods, and classes.
+        */
+    }
+}
