79fa2400cac36fbe617f141eccccf2c23a182633
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-18 17:32:02 +0530

    Problems added

---
+++ 4_OOPS/10_Static_And_Final_Keywords/Problems.java
@@ -0,0 +1,14 @@
+public class Problems {
+    public static void main(String[] args) {
+    /*
+    Practice Problems
+    -----------------
+        1) Create a class Counter with a static variable count.
+        -> Increment count in the constructor and display it.
+        2) Make a static utility method add(int a, int b) that returns their sum.
+        3) Create a class with a static block to initialize a static variable and print a message.
+        4) Declare a final variable MAX_SPEED = 120 inside a class Vehicle and try to change it (observe error).
+        5) Create a final method display() in a class Parent and attempt to override it in a subclass.
+    */
+    }
+}
