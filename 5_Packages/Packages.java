2cc43dd63ea0f6e8fb3f193af0ea415a37c3f863
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-20 10:52:44 +0530

    Packages concept added

---
+++ 5_Packages/Packages.java
@@ -0,0 +1,28 @@
+public class Packages {
+    public static void main(String[] args) {
+        //Packages
+        /*
+            -> A package is a group of related classes, interfaces, and sub-packages.
+            -> It helps organize code, avoid name conflicts, and makes maintenance easier.
+        */
+
+        //Types of packages
+        /*
+            1) Built-in Packages
+            2) User Defined Packages
+
+            1) Built-in Packages
+                -> Provided by Java, e.g., java.util, java.io, java.sql, java.time, etc.
+                -> We import these using the import statement.
+                -> Example: import java.util.Scanner;
+            EG:-
+            import java.util.Scanner; --> Here the dot represents subfile or subfolder i.e the Scanner is a file in util folder and the util foder present in java folder
+            public class Main{
+                public static void main(String[] args){
+                    Scanner sc = new Scanner(System.in);  --> we are using the scanner file by creating the object for that
+                    int a = sc.nextInt();
+                }
+            }
+        */
+    }
+}
