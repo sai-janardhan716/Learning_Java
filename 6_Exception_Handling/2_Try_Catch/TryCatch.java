aae9e3a3866e9518ec572775d37904caf19c61b0
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-23 11:38:19 +0530

    Try catch definition added

---
+++ 6_Exception_Handling/2_Try_Catch/TryCatch.java
@@ -0,0 +1,16 @@
+public class TryCatch {
+    public static void main(String[] args) {
+        /*
+            -> Try catch blocks are used to handle exceptions (runtime errors) so the program doesn’t crash.
+            -> The code that might throw an exception is placed inside the try block.
+            -> The catch block is used to handle the exception.
+
+            Syntax:
+            try {
+                // code that may throw an exception
+            } catch (ExceptionType e) {
+                // code to handle the exception
+            }
+        */
+    }    
+}
