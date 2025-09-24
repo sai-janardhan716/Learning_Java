b4a7bcf1db9d1c5d772d4ed113297f99cbd4228f
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-24 08:15:07 +0530

    Problem 4 solution added

--- 6_Exception_Handling/2_Try_Catch/Practice.java
+++ 6_Exception_Handling/2_Try_Catch/Practice.java
@@ -87,6 +87,17 @@ public class Practice {
                     }
                 }
             }
+
+            4)class NullPointer{
+                public static void main(String[] args) {
+                    try{
+                        String str=null;
+                        System.out.println(str.length());
+                    }catch (NullPointerException e){
+                        System.out.println("Error:String is null.");
+                    }
+                }
+            }
         */
     }
 }
