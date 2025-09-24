13048ff48365574ccce4be5b715924c7aa7e61c9
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-24 08:12:51 +0530

    Problem 2 solution added

--- 6_Exception_Handling/2_Try_Catch/Practice.java
+++ 6_Exception_Handling/2_Try_Catch/Practice.java
@@ -56,6 +56,21 @@ public class Practice {
                     }
                 }
             }
+
+            2)import java.util.*;
+            class ArrayIndex{
+                public static void main(String[] args){
+                    Scanner sc = new Scanner(System.in);
+                    int[] arr = {10,20,30,40,50};
+                    try{
+                        System.out.print("Enter index:");
+                        int idx = sc.nextInt();
+                        System.out.println("Element:" +arr[idx]);
+                    }catch (ArrayIndexOutOfBoundsException e){
+                        System.out.println("Error:Invalid index.");
+                    }
+                }
+            }
         */
     }
 }
