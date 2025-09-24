c373c5253d174de85e26f94a7ba31406d8d34586
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-24 08:25:29 +0530

    Updated

--- 6_Exception_Handling/2_Try_Catch/Practice.java
+++ 6_Exception_Handling/2_Try_Catch/Practice.java
@@ -39,116 +39,85 @@ public class Practice {
 
         //Solutions
         /*
-            1) import java.util.*;
-            class DivideByZero{
-                public static void main(String[] args){
-                    Scanner sc = new Scanner(System.in);
-                    try{
-                        System.out.print("Enter numerator:");
-                        int a = sc.nextInt();
-                        System.out.print("Enter denominator:");
-                        int b = sc.nextInt();
-                        int res = a / b;
-                        System.out.println("Result:" +res);
-                    }
-                    catch (ArithmeticException e){
-                        System.out.println("Error:Cannot divide by zero.");
-                    }
-                }
+            1)
+            try{
+                System.out.print("Enter numerator:");
+                int a = sc.nextInt();
+                System.out.print("Enter denominator:");
+                int b = sc.nextInt();
+                int res = a / b;
+                System.out.println("Result:" +res);
+            }
+            catch (ArithmeticException e){
+                System.out.println("Error:Cannot divide by zero.");
             }
 
-            2)import java.util.*;
-            class ArrayIndex{
-                public static void main(String[] args){
-                    Scanner sc = new Scanner(System.in);
-                    int[] arr = {10,20,30,40,50};
-                    try{
-                        System.out.print("Enter index:");
-                        int idx = sc.nextInt();
-                        System.out.println("Element:" +arr[idx]);
-                    }catch (ArrayIndexOutOfBoundsException e){
-                        System.out.println("Error:Invalid index.");
-                    }
-                }
+            2)
+            Scanner sc = new Scanner(System.in);
+            int[] arr = {10,20,30,40,50};
+            try{
+                System.out.print("Enter index:");
+                int idx = sc.nextInt();
+                System.out.println("Element:" +arr[idx]);
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Error:Invalid index.");
             }
 
-            3)import java.util.*;
-            class NumberFormat{
-                public static void main(String[] args){
-                    Scanner sc =new Scanner(System.in);
-                    try{
-                        System.out.print("Enter a number:");
-                        String s = sc.nextLine();
-                        int num = Integer.parseInt(s);
-                        System.out.println("Integer:" + num);
-                    } 
-                    catch(NumberFormatException e){
-                        System.out.println("Error:Not a valid integer");
-                    }
-                }
+            3)
+            Scanner sc =new Scanner(System.in);
+            try{
+                System.out.print("Enter a number:");
+                String s = sc.nextLine();
+                int num = Integer.parseInt(s);
+                System.out.println("Integer:" + num);
+            } 
+            catch(NumberFormatException e){
+                System.out.println("Error:Not a valid integer");
             }
 
-            4)class NullPointer{
-                public static void main(String[] args) {
-                    try{
-                        String str=null;
-                        System.out.println(str.length());
-                    }catch (NullPointerException e){
-                        System.out.println("Error:String is null.");
-                    }
-                }
+            4)
+            try{
+                String str=null;
+                System.out.println(str.length());
+            }catch (NullPointerException e){
+                System.out.println("Error:String is null.");
             }
 
-            5)import java.io.*;
-            class FileRead{
-                public static void main(String[] args){
-                    try{
-                        BufferedReader br = new BufferedReader(new FileReader("data.txt"));
-                        System.out.println(br.readLine());
-                        br.close();
-                    }catch (FileNotFoundException e) {
-                        System.out.println("Error: File not found.");
-                    }catch (IOException e){
-                        System.out.println("Error reading file");
-                    }
-                }
+            5)
+            try{
+                BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+                System.out.println(br.readLine());
+                br.close();
+            }catch (FileNotFoundException e) {
+                System.out.println("Error: File not found.");
+            }catch (IOException e){
+                System.out.println("Error reading file");
             }
 
-            6) import java.util.*;
-            class MultipleCatch{
-                public static void main(String[] args){
-                    Scanner sc = new Scanner(System.in);
-                    int[] arr = new int[2];
-                    try{
-                        System.out.print("Enter first number:");
-                        arr[0]=sc.nextInt();
-                        System.out.print("Enter second number:");
-                        arr[1] =sc.nextInt();
-                        System.out.println("Division: " + arr[0]/arr[1]);
-                    } catch(ArithmeticException e){
-                        System.out.println("Error:Division by zero");
-                    }catch (ArrayIndexOutOfBoundsException e){
-                        System.out.println("Error:Array index issue.");
-                    }
-                }
+            6)
+            int[] arr = new int[2];
+            try{
+                System.out.print("Enter first number:");
+                arr[0]=sc.nextInt();
+                System.out.print("Enter second number:");
+                arr[1] =sc.nextInt();
+                System.out.println("Division: " + arr[0]/arr[1]);
+            } catch(ArithmeticException e){
+                System.out.println("Error:Division by zero");
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Error:Array index issue.");
             }
 
-            7) import java.util.*;
-            class FinallyBlock{
-                public static void main(String[] args) {
-                    Scanner sc=new Scanner(System.in);
-                    try{
-                        System.out.print("Enter numerator:");
-                        int a = sc.nextInt();
-                        System.out.print("Enter denominator:");
-                        int b =sc.nextInt();
-                        System.out.println("Result: " + (a/b));
-                    } catch(ArithmeticException e){
-                        System.out.println("Error: Cannot divide by zero");
-                    } finally{
-                        System.out.println("Execution Completed");
-                    }
-                }
+            7) try{
+                System.out.print("Enter numerator:");
+                int a = sc.nextInt();
+                System.out.print("Enter denominator:");
+                int b =sc.nextInt();
+                System.out.println("Result: " + (a/b));
+            }catch(ArithmeticException e){
+                System.out.println("Error: Cannot divide by zero");
+            }finally{
+                System.out.println("Execution Completed");
             }
         */
     }
