9a2d6575a72e55cd9d55fa17656e01bf8bd18767
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-24 08:18:38 +0530

    Problem 7 solution added

--- 6_Exception_Handling/2_Try_Catch/Practice.java
+++ 6_Exception_Handling/2_Try_Catch/Practice.java
@@ -132,6 +132,24 @@ public class Practice {
                     }
                 }
             }
+
+            7) import java.util.*;
+            class FinallyBlock{
+                public static void main(String[] args) {
+                    Scanner sc=new Scanner(System.in);
+                    try{
+                        System.out.print("Enter numerator:");
+                        int a = sc.nextInt();
+                        System.out.print("Enter denominator:");
+                        int b =sc.nextInt();
+                        System.out.println("Result: " + (a/b));
+                    } catch(ArithmeticException e){
+                        System.out.println("Error: Cannot divide by zero");
+                    } finally{
+                        System.out.println("Execution Completed");
+                    }
+                }
+            }
         */
     }
 }
