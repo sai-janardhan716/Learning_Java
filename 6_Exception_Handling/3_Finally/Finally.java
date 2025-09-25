7a6d421d4b51c61745b7be5794e068b69fa178aa
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-25 07:56:45 +0530

    Finally block definition added

---
+++ 6_Exception_Handling/3_Finally/Finally.java
@@ -0,0 +1,17 @@
+public class Finally {
+    public static void main(String[] args) {
+        //Finally
+        /*
+            -> The finally block is used with try-catch to execute code 
+                that must run regardless of whether an exception occurs or not.
+            -> Syntax:
+                    try {
+                        // code that may throw an exception
+                    } catch (ExceptionType e) {
+                        // handle exception
+                    } finally {
+                        // cleanup code that always executes
+                    }
+        */
+    }
+}
