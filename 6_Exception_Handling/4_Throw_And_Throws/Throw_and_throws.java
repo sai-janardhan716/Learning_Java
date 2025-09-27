fd18d99daeb796b06ba5969d584214178a695c88
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-27 10:46:39 +0530

    Throw concept definition added

---
+++ 6_Exception_Handling/4_Throw_And_Throws/Throw_and_throws.java
@@ -0,0 +1,14 @@
+public class Throw_and_throws {
+    public static void main(String[] args) {
+        //Throw
+        /*
+            -> Used to explicitly throw a single exception (object of Exception class or its subclasses).
+            -> Used inside a method or block of code.
+            -> We throw an instance of an exception.
+            -> After throw statement, no code is executed in that block.
+
+            Syntax:-
+            throw new ExceptionType("error message");
+        */
+    }
+}
