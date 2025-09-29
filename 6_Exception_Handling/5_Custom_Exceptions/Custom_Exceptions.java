cb3a96d55935f80da987d796a1f23937b148942c
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-29 16:39:22 +0530

    Custom exception definition added

--- 6_Exception_Handling/5_Custom_Exceptions/Custom_Exceptions.java
+++ 6_Exception_Handling/5_Custom_Exceptions/Custom_Exceptions.java
@@ -1,5 +1,19 @@
 public class Custom_Exceptions {
    public static void main(String[] args) {
-    
+        /*
+            -> Java provides many built-in exceptions like ArithmeticException, NullPointerException, etc.
+            -> But sometimes we need our own exceptions to represent application-specific problems
+               (for example: "InsufficientFunds", "InvalidAge", "LowBattery", etc.).
+            -> These are called Custom Exceptions (or User-Defined Exceptions) means created by ours for our application purposes.
+            -> A custom exception is simply a class that extends Exception (checked) or RuntimeException (unchecked).
+            -> We can add:
+               -> Constructors to pass error messages.
+               -> Additional fields/methods if needed.
+            -> Checked vs Unchecked:
+               -> If you extend Exception (but not RuntimeException), it's checked 
+                  -> Must be declared using throws and handled with try-catch.
+               -> If you extend RuntimeException, it's unchecked
+                  -> No need to declare or handle explicitly.
+        */
    } 
 }
