dd566575642f059af032c511fb132a24d368ca8c
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-09-30 07:43:44 +0530

    Practice problems for custom exceptoions added

---
+++ 6_Exception_Handling/5_Custom_Exceptions/Practice.java
@@ -0,0 +1,48 @@
+public class Practice {
+    public static void main(String[] args) {
+/*
+        //Problems
+        1) Username Validation
+        -------------------
+        -> Create a custom unchecked exception InvalidUsernameException.
+        -> Method validate(String username) should throw it if:
+                - username length < 6 OR contains spaces.
+        -> In main(), read username from user and catch exception if thrown.
+
+        2) File Extension Checker
+        ----------------------
+        -> Define a custom checked exception InvalidFileExtensionException.
+        -> Write a method checkFile(String fileName) that throws this exception 
+            if file does not end with ".txt".
+        -> In main(), test with different file names.
+
+        3) Negative Number Square Root
+        ---------------------------
+        -> Create a custom checked exception NegativeNumberException.
+        -> Method findSquareRoot(double num) should throw NegativeNumberException 
+            if num is negative, otherwise return Math.sqrt(num).
+        -> Handle it in main().
+
+        4) Password Strength Checker
+        -------------------------
+        -> Create a custom unchecked exception WeakPasswordException.
+        -> Method validatePassword(String pass) should throw exception if:
+                - Length < 8
+                - Does not contain a digit
+                - Does not contain a special character
+        -> In main(), test different passwords and catch the exception to print a strong password hint.
+
+        5) College Admission
+        -----------------
+        -> Custom checked exception LowScoreException.
+        -> Method applyForAdmission(double marks) throws LowScoreException if marks < 60.
+        -> In main(), read marks and handle the exception.
+
+        6) Divide by Zero Custom
+        ---------------------
+        -> Define a custom unchecked exception DivisionByZeroException.
+        -> Method divide(int a, int b) should throw it if b == 0.
+        -> Handle in main() and display a clear error message.
+*/
+    }
+}
