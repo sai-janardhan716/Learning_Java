9c16a503116032fc10633c87a401cc5cf4595ba3
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-10-05 14:56:57 +0530

    Wrapper Casses definition added

---
+++ 7_Java_Collections_Frameworks/1_Wrapper_Classes/Wrapper_classes.java
@@ -0,0 +1,11 @@
+public class Wrapper_classes {
+    public static void main(String[] args) {
+        /*
+            -> Wrapper classes are object representations of primitive data types in Java.
+            -> They allow primitives (like int, char, double) to be used as objects, enabling them to work with Collections, Generics, and Object-oriented features.
+            Purpose:
+            -> To convert primitive data types into objects and vice versa.
+            -> Useful for working with data structures (like ArrayList, HashMap) that store objects only.
+        */
+    }
+}
