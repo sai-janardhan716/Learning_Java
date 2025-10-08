d18455e1dcb0f4e249c2ce986a8a9cddbfa35114
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-10-08 10:55:05 +0530

    Array list Example 2 added

--- 7_Java_Collections_Frameworks/ArrayLists/ArrayLists.java
+++ 7_Java_Collections_Frameworks/ArrayLists/ArrayLists.java
@@ -47,6 +47,29 @@ public class ArrayLists {
             fruits.remove(0);                                   // Remove first element
             System.out.println("After removal: " + fruits);
             System.out.println("List size: " + fruits.size());  // Returns length of arraylist
+
+            EG2:-
+            ArrayList<Integer> numbers = new ArrayList<>();
+            numbers.add(10);
+            numbers.add(20);
+            numbers.add(30);
+            numbers.add(40);
+            // Using for loop
+            System.out.println("Using for loop:");
+            for (int i = 0; i < numbers.size(); i++) {
+                System.out.println(numbers.get(i));
+            }
+            // Using for-each loop
+            System.out.println("Using for-each loop:");
+            for (int n : numbers) {
+                System.out.println(n);
+            }
+            // Using Iterator
+            System.out.println("Using Iterator:");
+            Iterator<Integer> it = numbers.iterator();
+            while (it.hasNext()) {
+                System.out.println(it.next());
+            }
         */
     }
 }
