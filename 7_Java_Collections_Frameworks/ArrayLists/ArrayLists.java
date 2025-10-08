57c980b238ca6a18171600700983a006f8ae5163
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-10-08 10:48:43 +0530

    Array list definition added

---
+++ 7_Java_Collections_Frameworks/ArrayLists/ArrayLists.java
@@ -0,0 +1,12 @@
+public class ArrayLists {
+    public static void main(String[] args) {
+        //ArrayList
+        /*
+            -> ArrayList is a part of the Java Collections Framework.
+            -> It is a resizable array implementation of the List interface.
+            -> Unlike arrays, ArrayList can grow or shrink dynamically.
+            -> It allows duplicate elements and maintains insertion order.
+            -> It can store elements of any data type (by using generics only).
+        */
+    }
+}
