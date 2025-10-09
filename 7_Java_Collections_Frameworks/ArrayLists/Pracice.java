9ef7a46d774e86f70cc0f9d49212156bd012a00c
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-10-09 09:14:37 +0530

    Arraylist problems added

---
+++ 7_Java_Collections_Frameworks/ArrayLists/Pracice.java
@@ -0,0 +1,42 @@
+public class Pracice {
+    public static void main(String[] args) {
+        //Problems
+        /*
+            1) AddAndDisplay
+            • Create an ArrayList of Strings to store student names.
+            • Add 5 names to the list.
+            • Display all names using a loop.
+
+            2) RemoveAndUpdate
+            • Create an ArrayList of Integers.
+            • Add 5 numbers into it.
+            • Remove the second element.
+            • Update the last element with a new value.
+            • Display the updated list.
+
+            3) SearchElement
+            • Create an ArrayList of Strings.
+            • Add few names into it.
+            • Read a name from the user.
+            • Check whether it exists in the list or not and display appropriate message.
+
+            4) SortList
+            • Create an ArrayList of Integers.
+            • Add random numbers into it.
+            • Sort the list in ascending order using Collections.sort().
+            • Display the sorted list.
+
+            5) ReverseList
+            • Create an ArrayList of Integers.
+            • Add elements into it.
+            • Reverse the list using Collections.reverse().
+            • Display both original and reversed lists.
+
+            6) ConvertToArray
+            • Create an ArrayList of Strings.
+            • Add color names into it.
+            • Convert the ArrayList into an array.
+            • Display all elements of the array.
+        */
+    }
+}
