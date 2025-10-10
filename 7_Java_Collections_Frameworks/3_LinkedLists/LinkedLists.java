40e05fac073dacb736cb4646aac29754d67ef5d3
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-10-10 07:21:07 +0530

    LinkedList definition added

---
+++ 7_Java_Collections_Frameworks/3_LinkedLists/LinkedLists.java
@@ -0,0 +1,15 @@
+public class LinkedLists {
+    public static void main(String[] args) {
+        //LinkedList
+        /*
+            -> LinkedList is part of the java.util package.
+            -> It is a doubly linked list implementation of the List and Deque interfaces.
+            -> Unlike ArrayList (which uses a dynamic array), LinkedList stores elements as nodes.
+            -> Each node contains:
+                   - Data
+                   - Address of previous node
+                   - Address of next node
+            -> It is useful when frequent insertions and deletions are required.
+        */
+    }    
+}
