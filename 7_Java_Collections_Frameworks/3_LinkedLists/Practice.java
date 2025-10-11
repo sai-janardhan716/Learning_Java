ac29419f3132d3681079b0ac18f476b0a2c7e373
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-10-11 19:20:35 +0530

    LinkedList Problems Added

---
+++ 7_Java_Collections_Frameworks/3_LinkedLists/Practice.java
@@ -0,0 +1,25 @@
+public class Practice {
+    public static void main(String[] args){
+        //Problems
+        /*
+            1) Insert Elements
+            -------------------
+            • Create a LinkedList of Strings named cities.
+            • Add 5 city names to the list.
+            • Insert a new city at the beginning and another at the end.
+            • Print the LinkedList after all insertions.
+
+            2) Delete Elements
+            -------------------
+            • Create a LinkedList of integers with 5 elements.
+            • Remove the first element, last element, and one element at a specific index.
+            • Print the LinkedList after each deletion to observe the change.
+
+            3) Update Elements
+            -------------------
+            • Create a LinkedList of student names.
+            • Update the second and last student names using the set() method.
+            • Print the LinkedList before and after updating.
+        */
+    }
+}
