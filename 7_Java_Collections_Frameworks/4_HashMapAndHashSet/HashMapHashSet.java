8bf63a9b073e08b51911e16e3dbeabb36662b5f0
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-10-13 08:14:31 +0530

    Hashmap and hashset definitions added

---
+++ 7_Java_Collections_Frameworks/4_HashMapAndHashSet/HashMapHashSet.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class HashMapHashSet {
+    public static void main(String[] args) {
+        //HashMap
+        /* 
+            -> A HashMap in Java is a part of the java.util package.
+            -> It is used to store data in key-value pairs, where each key is unique and maps to a specific value.
+            -> It uses a hash table to store and retrieve values efficiently (almost constant time — O(1)).
+        */
+
+        //HashSet
+        /* 
+            -> A HashSet is a collection that contains unique elements only.
+            -> It does not allow duplicates, and it doesn’t maintain insertion order.
+            -> Internally, a HashSet uses a HashMap to store elements.
+        */
+
+        // Syntax
+        // HashMap<KeyType, ValueType> map = new HashMap<>();
+        // HashSet<Type> set = new HashSet<>();
+    }    
+}
