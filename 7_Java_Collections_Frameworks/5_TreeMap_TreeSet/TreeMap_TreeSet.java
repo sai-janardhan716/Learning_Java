62791b46df52dd8f96d7df81eae6cc3f0f3c218a
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-10-18 13:15:31 +0530

    Treeset Definition added

---
+++ 7_Java_Collections_Frameworks/5_TreeMap_TreeSet/TreeMap_TreeSet.java
@@ -0,0 +1,17 @@
+public class TreeMap_TreeSet{
+    public static void main(String[] args) {
+        /*
+            -> Both are part of the java.util package.
+            -> They are similar to HashSet and HashMap but maintain the elements in sorted order.
+            -> Internally, both use a self-balancing Red-Black Tree data structure.
+        */
+
+        // TreeSet
+        /* 
+            -> TreeSet is used to store unique elements in a sorted (ascending) order.
+            -> It does not allow duplicate values.
+            -> Elements must be Comparable (e.g., Integer, String).
+            -> It does not allow null elements.
+        */
+    }
+}
