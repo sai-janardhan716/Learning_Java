9d774057ca4b56e2e234ba36b2f9ddf27192f455
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-10-20 11:48:28 +0530

    Stack Definition added

---
+++ 7_Java_Collections_Frameworks/6_Queue_Stack/StackQueue.java
@@ -0,0 +1,18 @@
+public class StackQueue {
+    public static void main(String[] args) {
+        /*
+            -> Both Queue and Stack are linear data structures in Java.
+            -> They are used to store and process elements in different orders.
+        */
+
+        // Stack - Last In First Out (LIFO)
+        /*
+            -> In programming, Stack follows the same rule:
+                  The element that is added last will be removed first.
+            -> Imagine a stack of plates in your kitchen.
+                 - You put a plate on top -> that’s push().
+                 - You remove the top plate -> that’s pop().
+                 - You can only take or see the top plate, not the middle ones.
+        */
+    }
+}
