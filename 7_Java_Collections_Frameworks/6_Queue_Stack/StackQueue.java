9b89efab4d708ef88c69882c69bbe42b5643bea6
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-10-20 11:49:21 +0530

    Stack Definition added

--- 7_Java_Collections_Frameworks/6_Queue_Stack/StackQueue.java
+++ 7_Java_Collections_Frameworks/6_Queue_Stack/StackQueue.java
@@ -13,6 +13,10 @@ public class StackQueue {
                  - You put a plate on top -> that’s push().
                  - You remove the top plate -> that’s pop().
                  - You can only take or see the top plate, not the middle ones.
+            -> Example of Real Life:
+                1) Undo feature in text editors,
+                2) Browser back button,
+                3) Function call stack in Java.
         */
     }
 }
