f6e28720d25733b06572505d61f5639f317e7907
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-10-21 09:43:54 +0530

    Stack and queue problems added

---
+++ 7_Java_Collections_Frameworks/6_Queue_Stack/Practice.java
@@ -0,0 +1,29 @@
+public class Practice {
+    public static void main(String[] args) {
+        // Problems
+        /*
+        1) StackPushPop
+               • Create a Stack of integers.
+               • Push few elements.
+               • Pop the top element and display the stack before and after.
+
+            2) ReverseString
+               • Read a string from user.
+               • Push each character to stack.
+               • Pop and form reversed string.
+
+            3) BalancedParentheses
+               • Check whether given parentheses are balanced or not using Stack.
+               • Example: ({[]}) -> Balanced, ({[}) -> Not Balanced.
+
+            4) QueueOperations
+               • Create a Queue.
+               • Add few elements.
+               • Remove one element and show queue before and after.
+
+            5) ReverseQueue
+               • Create a Queue of integers.
+               • Use a Stack to reverse all elements in the queue.
+        */
+    }    
+}
