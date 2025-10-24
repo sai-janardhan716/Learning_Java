bbb81d33fb779e9c77a46506da70ab5c270fdadc
sai-janardhan716 <saijanardhanalapati@gmail.com>
2025-10-24 09:54:24 +0530

    File handling in java added

---
+++ 8_Others/1_File_Handling/FileHandling.java
@@ -0,0 +1,13 @@
+public class FileHandling {
+    public static void main(String[] args) {
+        // File Handling in java
+        /*
+            -> File handling in Java allows us to work with files on the disk - we can create, read, write, and delete files.
+            -> Java provides several classes in the java.io package:
+                -> File
+                -> FileReader
+                -> FileWriter
+                -> BufferedReader / BufferedWriter (for efficient reading/writing)
+        */
+    }    
+}
